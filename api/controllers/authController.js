1f92443ea7cae7aa755d416e013ef48b04e301d6
Tejas Jadhav <tj2972001@gmail.com>
2020-09-22 11:40:26 +0530

    changed authController res.cookie()

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -12,14 +12,13 @@ const signToken = (id) => {
   })
 }
 
-const createSendToken = (user, code, req, res) => {
+const createSendToken = (user, code, res) => {
   const token = signToken(user._id)
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   }
   // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
   res.cookie('jwt', token, cookieOptions)
