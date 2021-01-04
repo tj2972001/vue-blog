96e3a40b1ab0fbd5b1d38328e6f306bd9e33fbdf
tejas <tj2972001@gmail.com>
2021-01-04 22:19:37 +0530

    make it better

--- api/app.js
+++ api/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next()
 })
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(process.env.DATABASE_REMOTE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
