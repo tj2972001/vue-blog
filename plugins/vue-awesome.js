99f618a39ada17549c0c4a3971d6666285d7e56c
tejas <tj2972001@gmail.com>
2021-06-03 00:47:02 +0530

    a

--- plugins/vue-awesome.js
+++ plugins/vue-awesome.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-import Icon from 'vue-awesome/components/Icon.vue'
-require('vue-awesome/icons')
+import Vue from "vue";
+import Icon from "vue-awesome/components/Icon.vue";
+require("vue-awesome/icons");
 
-Vue.component('fa-icon', Icon)
+Vue.component("fa-icon", Icon);
